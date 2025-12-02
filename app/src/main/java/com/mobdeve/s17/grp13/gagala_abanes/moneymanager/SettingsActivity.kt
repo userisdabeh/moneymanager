406f696349491e73a20b4e83cc46cfd96846ff8d
nzowski <160837131+nzowski@users.noreply.github.com>
2025-12-02 22:31:50 +0800

    not permanent theme change functionality (settings page only)

--- app/src/main/java/com/mobdeve/s17/grp13/gagala_abanes/moneymanager/SettingsActivity.kt
+++ app/src/main/java/com/mobdeve/s17/grp13/gagala_abanes/moneymanager/SettingsActivity.kt
@@ -13,45 +13,47 @@ class SettingsActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        val rootLayout = findViewById<android.view.View>(R.id.settingsRoot)
+
         val spinner: Spinner = findViewById(R.id.theme_dropdwn)
         val adapter = ArrayAdapter.createFromResource(
             this,
-            R.array.theme_options, // defined in res/values/strings.xml
+            R.array.theme_options,
             android.R.layout.simple_spinner_item
         )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
+        spinner.setOnItemSelectedListener(object : android.widget.AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: android.widget.AdapterView<*>, view: android.view.View?, position: Int, id: Long) {
+                when (spinner.selectedItem.toString()) {
+                    "Default" -> rootLayout.setBackgroundColor(getColor(R.color.app_default_bg))
+                    "Light"   -> rootLayout.setBackgroundColor(getColor(R.color.app_light_bg))   // white
+                    "Dark"    -> rootLayout.setBackgroundColor(getColor(R.color.app_dark_bg))    // dark gray
+                }
+            }
+
+            override fun onNothingSelected(parent: android.widget.AdapterView<*>) {}
+        })
+
         val deleteButton: Button = findViewById(R.id.deldata)
         deleteButton.setOnClickListener {
-            // Build confirmation dialog
             AlertDialog.Builder(this)
                 .setTitle("Confirm Delete")
                 .setMessage("Are you sure you want to delete all data?")
-                .setPositiveButton("Yes") { dialog, _ ->
-                    // TODO: Add code to delete your data here
-                    dialog.dismiss()
-                }
-                .setNegativeButton("No") { dialog, _ ->
-                    dialog.dismiss()
-                }
+                .setPositiveButton("Yes") { dialog, _ -> dialog.dismiss() }
+                .setNegativeButton("No") { dialog, _ -> dialog.dismiss() }
                 .show()
         }
 
-        //bottom ribbon functionality
         val bottomRibbon: BottomRibbon = findViewById(R.id.bottomRibbon)
         bottomRibbon.btnHome.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
-
-        bottomRibbon.btnSettings.setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        }
-
         bottomRibbon.btnTag.setOnClickListener {
             startActivity(Intent(this, ETagsActivity::class.java))
         }
-        //end of bottom ribbon functionality
-    }// end of onCreate
+    }
 }
+
 
