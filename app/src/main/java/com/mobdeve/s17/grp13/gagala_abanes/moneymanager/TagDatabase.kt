d47a9738fed6d9bd1821dca50c6018e6a0c718d1
nzowski <160837131+nzowski@users.noreply.github.com>
2025-12-05 13:04:00 +0800

    updated db version

--- app/src/main/java/com/mobdeve/s17/grp13/gagala_abanes/moneymanager/TagDatabase.kt
+++ app/src/main/java/com/mobdeve/s17/grp13/gagala_abanes/moneymanager/TagDatabase.kt
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class TagDatabase(context: Context) :
-    SQLiteOpenHelper(context, "tags.db", null, 3) {
+    SQLiteOpenHelper(context, "tags.db", null, 4) {
 
     override fun onCreate(db: SQLiteDatabase) {
         //create exp tags table
