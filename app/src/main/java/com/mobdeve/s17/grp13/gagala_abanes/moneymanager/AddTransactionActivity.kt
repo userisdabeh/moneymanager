eca7ea3ec8c14cb9458b76afe011d9ccdc6dcfee
nzowski <160837131+nzowski@users.noreply.github.com>
2025-12-07 22:09:00 +0800

    added light theme and bottom ribbon functionalities

--- app/src/main/java/com/mobdeve/s17/grp13/gagala_abanes/moneymanager/AddTransactionActivity.kt
+++ app/src/main/java/com/mobdeve/s17/grp13/gagala_abanes/moneymanager/AddTransactionActivity.kt
@@ -22,6 +22,14 @@ class AddTransactionActivity: ComponentActivity() {
 
     fun Int.dpToPx(): Int = (this * resources.displayMetrics.density).toInt()
 
+    private fun applyTheme(layout: View, mode: String) {
+        when (mode) {
+            "Default" -> layout.setBackgroundColor(getColor(R.color.app_default_bg))
+            "Light" -> layout.setBackgroundColor(getColor(R.color.app_light_bg))
+            "Dark" -> layout.setBackgroundColor(getColor(R.color.app_dark_bg))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_transaction)
@@ -113,7 +121,27 @@ class AddTransactionActivity: ComponentActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 3)
         recyclerView.adapter = photoAdapter
 
+        //light mode stuff
+        val prefs = getSharedPreferences("app_theme", MODE_PRIVATE)
+        val savedTheme = prefs.getString("theme", "Default")!!
 
+        val rootLayout = findViewById<View>(R.id.rootLayout)
+        applyTheme(rootLayout, savedTheme)
+
+        //bottom ribbon functionality
+        val bottomRibbon: BottomRibbon = findViewById(R.id.bottomRibbon)
+        bottomRibbon.btnHome.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        bottomRibbon.btnSettings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+
+        bottomRibbon.btnTag.setOnClickListener {
+            startActivity(Intent(this, ETagsActivity::class.java))
+        }
+        //end of bottom ribbon functionality
     }
 
     private fun showPhotoOptions() {
